bd1a0f4106ffa5348e6577f536f8bb202d25801f
Iando Rafid <iando.rafid@gmail.com>
2017-02-24 21:36:33 +0100

    Implementation Trumpet, Flute, IInstrument

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/IInstrument.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.heigvd.res.lab00;
+
+/**
+ *
+ * @author fidimala
+ */
+public interface IInstrument{
+    public String play();
+    public int getSoundVolume();
+    public String getColor();
+}

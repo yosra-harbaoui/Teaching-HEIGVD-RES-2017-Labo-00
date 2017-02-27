b4952a39b6117076b1bb2981db396afd28b5b972
Antoine <antoine.nourazar@heig-vd.ch>
2017-02-27 12:30:28 +0100

    Add specs for Lute, error fixed

--- Lab00App-build/Lab00App-tests/src/test/java/ch/heigvd/res/lab00/LuteTest.java
+++ Lab00App-build/Lab00App-tests/src/test/java/ch/heigvd/res/lab00/LuteTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 public class LuteTest {
 	
-	public void aLuteShouldMakeTingTing {
+	public void aLuteShouldMakeTingTing() {
 		
 		IInstrument lute = new Lute();
 		String sound = lute.play();

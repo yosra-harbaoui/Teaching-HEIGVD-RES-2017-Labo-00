c44abfcc3500f2ba629c8ba50c407df1a105bd12
Julien Brêchet <Julien Brchet>
2017-02-27 21:04:05 +0100

    var corDesAlpes renamed to alphorn. IDE comments removed.

--- Lab00App-build/Lab00App-tests/src/test/java/ch/heigvd/res/lab00/AlphornTest.java
+++ Lab00App-build/Lab00App-tests/src/test/java/ch/heigvd/res/lab00/AlphornTest.java
@@ -4,21 +4,11 @@ import org.junit.Assert;
 import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Julien Brêchet
- */
 public class AlphornTest {
    @Test
    public void alphornDoBrooo() {
-      IInstrument corDesAlpes = new Alphorn();
-      String sound = corDesAlpes.play();
+      IInstrument alpHorn = new Alphorn();
+      String sound = alpHorn.play();
       Assert.assertEquals("Brooo", sound);
    }
 }

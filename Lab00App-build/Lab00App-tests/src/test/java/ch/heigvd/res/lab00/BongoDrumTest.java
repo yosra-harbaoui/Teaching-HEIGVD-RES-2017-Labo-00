7193ba1d7dc5e781cecd26abb6dc4e2e9d9b047b
Gigon Quentin <gigon.quentin@gmail.com>
2017-02-27 20:21:55 +0100

    changed file according to request

---
+++ Lab00App-build/Lab00App-tests/src/test/java/ch/heigvd/res/lab00/BongoDrumTest.java
@@ -0,0 +1,17 @@
+package ch.heigvd.res.lab00;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BongoDrumTest {
+
+  public BongoDrumTest(){}
+
+  public String aBongoDrumShouldMakeBongoBongo()
+  {
+    BongoDrum bongo = new BongoDrum();
+    String message = bongo.sound();
+    assertEquals(message, "bongo bongo");
+  }
+}
+
--- Lab00App-build/Lab00App-tests/src/test/java/ch/heigvd/res/lab00/BongodrumTest.java
+++
@@ -1,4 +0,0 @@
-public String sound()
-    {
-      return "Bongo Bongo !";
-    }

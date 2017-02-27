3f2d0e455d959a4469301bd6450cfa1b797848d0
psrochat <pierre.samuel.rochat@gmail.com>
2017-02-27 20:06:22 +0100

    Add specs for Saxophone

--- Lab00App-build/Lab00App-tests/src/test/java/ch/heigvd/res/lab00/SaxophoneTest.java
+++ Lab00App-build/Lab00App-tests/src/test/java/ch/heigvd/res/lab00/SaxophoneTest.java
@@ -2,17 +2,6 @@ package ch.heigvd.res.lab00;
 
 import org.junit.Assert;
 
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author pierre-samuelrochat
- */
 public class SaxophoneTest {
     
     public void aSaxophoneShouldMakePouetPouet() {

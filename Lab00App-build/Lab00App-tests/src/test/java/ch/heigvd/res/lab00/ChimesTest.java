95a0e1ce1c9e2a4aefffeeb8d47acfa9d12d2142
Colin Lavanchy <colin.lavanchy@heig-vd.ch>
2017-02-27 21:02:31 +0100

    Uncommented code

--- Lab00App-build/Lab00App-tests/src/test/java/ch/heigvd/res/lab00/ChimesTest.java
+++ Lab00App-build/Lab00App-tests/src/test/java/ch/heigvd/res/lab00/ChimesTest.java
@@ -6,12 +6,12 @@ import org.junit.Test;
  * Created by celestius on 27.02.17.
  */
 public class ChimesTest {
-    /*
+
     @Test
     public void aChimesShouldDoTheRightSound() {
         IInstrument chimes = new Chimes();
         String sound = trumpet.play();
         Assert.assertEquals("dring", sound);
     }
-    */
+    
 }

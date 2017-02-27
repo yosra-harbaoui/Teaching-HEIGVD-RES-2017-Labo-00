975ac0cf2c7f20195e748e129f022bde0fdb627a
Colin Lavanchy <colin.lavanchy@heig-vd.ch>
2017-02-27 11:57:11 +0100

    Chimes test added : should make dring

---
+++ Lab00App-build/Lab00App-tests/src/test/java/ch/heigvd/res/lab00/ChimesTest.java
@@ -0,0 +1,17 @@
+package ch.heigvd.res.lab00;
+
+import org.junit.Test;
+
+/**
+ * Created by celestius on 27.02.17.
+ */
+public class ChimesTest {
+    /*
+    @Test
+    public void aChimesShouldDoTheRightSound() {
+        IInstrument chimes = new Chimes();
+        String sound = trumpet.play();
+        Assert.assertEquals("dring", sound);
+    }
+    */
+}

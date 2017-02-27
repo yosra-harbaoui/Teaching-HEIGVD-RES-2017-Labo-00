072a6c80caa602d1039a64be1357ec205d9c8b4e
P-B Monaco <pierre-benjamin.monaco@heig-vd.ch>
2017-02-27 16:52:44 +0100

    Add specs for ElectricGuitar

--- Lab00App-build/Lab00App-tests/src/test/java/ch/heigvd/res/lab00/ElectricGuitarTest.java
+++ Lab00App-build/Lab00App-tests/src/test/java/ch/heigvd/res/lab00/ElectricGuitarTest.java
@@ -19,25 +19,30 @@ public class ElectricGuitarTest {
     
   @Test
   public void anElectricGuitarShouldMakeWaaWaaaa() {
-    IInstrument eGuitar = new ElectricGuitar();
+    ElectricGuitar eGuitar = new ElectricGuitar();
     String sound = eGuitar.play();
     Assert.assertEquals("waawaaaa", sound);
   }
   
   @Test
-  public void anElectricGuitarShouldBeLouderThanASteelDrum() {
-    IInstrument eGuitar = new ElectricGuitar();
-    IInstrument steelDrum = new SteelDrum();
-    int eGuitarVolume = eGuitar.getSoundVolume();
-    int steelDrumVolume = steelDrum.getSoundVolume();
-    Assert.assertTrue(eGuitarVolume > steelDrumVolume);
+  public void anElectricGuitarShouldHaveAJack() {
+    ElectricGuitar eGuitar = new ElectricGuitar();
+    String connector = eGuitar.getConnector();
+    Assert.assertNotNull(connector);
   }
 
   @Test
   public void anElectricGuitarShouldHaveSixOrSevenStrings() {
-    IInstrument eGuitar = new ElectricGuitar();
+    ElectricGuitar eGuitar = new ElectricGuitar();
     int nbString = eGuitar.getStringNumber();
     Assert.assertTrue(nbString == 6 || nbString == 7);
+  }
+ 
+  @Test
+  public void anElectricGuitarShouldHaveAConnector() {
+    ElectricGuitar eGuitar = new ElectricGuitar();
+    ElectricGuitar.Connector connector = eGuitar.getConnector();
+    Assert.assertNotNull(eGuitar);
   }
  
 }

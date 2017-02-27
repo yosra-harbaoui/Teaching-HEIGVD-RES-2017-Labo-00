d9410e29354c907c65af1414874b69956f55be44
Benoit Gianinetti <benoit.gianinetti@gmail.com>
2017-02-27 12:04:04 +0100

    Add implementation for bel

---
+++ Lab00App-build/Lab00App-tests/src/test/java/ch/heigvd/res/lab00/BellTest.java
@@ -0,0 +1,15 @@
+package ch.heigvd.res.labo00;
+
+import org.junit.Assert;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Test;
+
+public class BellTest {
+  @Test
+  public void aTrumpetShouldMakePouet() {
+    IInstrument trumpet = new Trumpet();
+    String sound = trumpet.play();
+    Assert.assertEquals("bell", sound);
+  }
+}
+

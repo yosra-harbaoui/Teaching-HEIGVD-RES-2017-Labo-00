6d317e40d6ab423e27206091f97a0ef5ca03e3bb
chaymae_mb <chaymae_mb@bitbucket.org>
2017-02-28 12:04:57 +0100

    Add specs for Clavichord

--- Lab00App-build/Lab00App-tests/src/test/java/ch/heigvd/res/lab00/ClavichordTest.java
+++ Lab00App-build/Lab00App-tests/src/test/java/ch/heigvd/res/lab00/ClavichordTest.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ch.heigvd.res.lab00;
 
 import org.junit.Assert;
@@ -16,8 +12,8 @@ public class ClavichordTest {
     
       @Test
   public void aClavichordShouldMakeTing() {
-    IInstrument harmonica = new Harmonica();
-    String sound = harmonica.play();
+    IInstrument clavichord = new Clavichord();
+    String sound = clavichord.play();
     Assert.assertEquals("ting", sound);
   }
   

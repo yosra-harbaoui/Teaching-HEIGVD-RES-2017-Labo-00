24ada960208f117b0a5e57c3c69fc6da35835732
Di Pietro Adrian <adrian.dipietro@heig-vd.ch>
2017-02-28 11:48:49 +0000

    Modif BanjoTest for pull request

--- Lab00App-build/Lab00App-tests/src/test/java/ch/heigvd/res/lab00/BanjoTest.java
+++ Lab00App-build/Lab00App-tests/src/test/java/ch/heigvd/res/lab00/BanjoTest.java
@@ -1,13 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ch.heigvd.res.lab00;
+
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
  *
- * @author Adrian
+ * @author Di Pietro Adrian
  */
 public class BanjoTest {
     @Test

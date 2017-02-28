0fe3442a00019390bbdfeea158b9e3836e28236c
vagrant <vagrant@RES>
2017-02-28 17:45:39 +0000

    Add specs for Calliope

--- Lab00App-build/Lab00App-tests/src/test/java/ch/heigvd/res/lab00/CalliopeTest.java
+++ Lab00App-build/Lab00App-tests/src/test/java/ch/heigvd/res/lab00/CalliopeTest.java
@@ -1,10 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ch.heigvd.res.lab00;
 
+import org.junit.Assert;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Test;
 /**
  *
  * @author Yosra

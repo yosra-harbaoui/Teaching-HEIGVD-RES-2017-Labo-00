b072c4c61b7ae1bd9f8e5416a810a0094b0f7076
edwardransome <edward.ransome@heig-vd.ch>
2017-02-28 10:33:56 +0100

    Update pipaTest

--- Lab00App-build/Lab00App-tests/src/test/java/ch/heigvd/res/lab00/PipaTest.java
+++ Lab00App-build/Lab00App-tests/src/test/java/ch/heigvd/res/lab00/PipaTest.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ch.heigvd.res.lab00;
 
 import org.junit.Assert;
 import org.junit.Test;
 
 /**
- *
+ * Tests the Pipa instrument 
  * @author Eddie
  */
 public class PipaTest {

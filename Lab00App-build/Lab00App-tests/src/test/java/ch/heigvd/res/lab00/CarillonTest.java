e4403c64b2b027d1950bea587ef4583fd225a10b
Sydney Hauke <sydney.hauke@gmail.com>
2017-02-28 14:27:16 +0100

    Changed class name

--- Lab00App-build/Lab00App-tests/src/test/java/ch/heigvd/res/lab00/CarillonTest.java
+++ Lab00App-build/Lab00App-tests/src/test/java/ch/heigvd/res/lab00/CarillonTest.java
@@ -2,7 +2,7 @@ package ch.heigvd.res.lab00;
 
 /** Test class for testing class Carillon
  */
-public class Carillon {
+public class CarillonTest {
     @Test
     public void CarillonShouldMakeDong() {
         IInstrument carillon = new Carillon();

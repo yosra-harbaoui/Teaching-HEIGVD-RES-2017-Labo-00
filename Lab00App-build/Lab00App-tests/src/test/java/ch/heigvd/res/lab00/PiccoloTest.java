71f113a918a121f8263747bcf86cc1e95dfcb568
Iando Rafid <iando.rafid@gmail.com>
2017-02-28 14:31:47 +0100

    Add specs for Piccolo v1

--- Lab00App-build/Lab00App-tests/src/test/java/ch/heigvd/res/lab00/PiccoloTest.java
+++ Lab00App-build/Lab00App-tests/src/test/java/ch/heigvd/res/lab00/PiccoloTest.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ch.heigvd.res.lab00;
 
 import org.junit.Assert;
@@ -11,7 +7,7 @@ import org.junit.Test;
 
 /**
  *
- * @author fidimala
+ * @author Rafidimalala Iando
  */
 public class PiccoloTest {
       @Test

aa8decfa26b4b3630943d0ba27306252347295e5
Lawrence Stalder <lawrence.stalder@heig-vd.ch>
2017-03-01 11:37:11 +0100

    Fixed the spec class because it was not compiling (3 stupid error left in the code) and add implementation for Bass

--- Lab00App-build/Lab00App-tests/src/test/java/ch/heigvd/res/lab00/BassTest.java
+++ Lab00App-build/Lab00App-tests/src/test/java/ch/heigvd/res/lab00/BassTest.java
@@ -23,9 +23,9 @@ public class BassTest {
 	}
 	
 	@Test
-	public void bassShouldMakeBoumBoum {
+	public void bassShouldMakeBoumBoum() {
 		IInstrument bass = new Bass();
 		String sound = bass.play();
-		Assert.assertEquals(sound, "boum boum");
+		assertEquals(sound, "boum boum");
 	}
 }

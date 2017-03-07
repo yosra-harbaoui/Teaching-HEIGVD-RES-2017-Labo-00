7d6df59ea56e227178199012d9ec697e2b27d40b
Mathias Gilson <mathias.gilson@outlook.com>
2017-03-07 14:05:38 +0100

    implementation of ElectricOrgan

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/ElectricOrgan.java
@@ -0,0 +1,17 @@
+package ch.heigvd.res.lab00;
+
+/**
+ *
+ * @author Mathias Gilson
+ */
+public class ElectricOrgan implements IInstrument {
+
+    public ElectricOrgan() {
+
+    }
+
+    public String play() {
+        return "Dvuuuuund";
+    }
+
+}

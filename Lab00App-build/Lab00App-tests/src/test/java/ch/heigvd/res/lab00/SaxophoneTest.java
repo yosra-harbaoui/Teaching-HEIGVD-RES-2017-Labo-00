72fe006847d7e6e3035f5414f67d3ed9b5c503b0
psrochat <pierre.samuel.rochat@gmail.com>
2017-03-08 09:12:12 +0100

    Fixes #372

--- Lab00App-build/Lab00App-tests/src/test/java/ch/heigvd/res/lab00/SaxophoneTest.java
+++ Lab00App-build/Lab00App-tests/src/test/java/ch/heigvd/res/lab00/SaxophoneTest.java
@@ -1,9 +1,13 @@
 package ch.heigvd.res.lab00;
 
 import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SaxophoneTest {
-    
+
+    @Test
     public void aSaxophoneShouldMakePouetPouet() {
         IInstrument saxophone = new Saxophone();
         String sound = saxophone.play();
